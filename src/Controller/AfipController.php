d391ae4d5996a1df25037782fc13c25d9c1905c7
debianDev <sistemas@personasjuridicas.gob.ar>
2022-12-10 16:46:56 -0300

    add organismo

--- src/Controller/AfipController.php
+++ src/Controller/AfipController.php
@@ -74,6 +74,89 @@ class AfipController
         
     }
     
+    public function getWSA7()
+    {
+        $facturaE = $this->afip->__get('RegisterScopeSeven');
+        
+        return $facturaE;
+        //$facturaE = json_decode(json_encode($facturaE->GetVoucherTypes()), true);
+        
+    }
+    
+    public function checkHomonimia($data)
+    {
+        $wsa7 = $this->afip->__get('RegisterScopeSeven');
+        $res = $wsa7->checkHomonimia($data);
+        
+        
+        return $res;
+        //$facturaE = json_decode(json_encode($facturaE->GetVoucherTypes()), true);
+        
+    }
+    
+    public function getValidatePersonaWSA7($data)
+    {
+        $wsa7 = $this->afip->__get('RegisterScopeSeven');
+        $res = $wsa7->validatePersona($data);
+        
+        
+        return $res;
+        //$facturaE = json_decode(json_encode($facturaE->GetVoucherTypes()), true);
+        
+    }
+    
+    public function createPersonaWSA7($data)
+    {
+        $wsa7 = $this->afip->__get('RegisterScopeSeven');
+        $res = $wsa7->createPersona($data);
+        
+        
+        return $res;
+        //$facturaE = json_decode(json_encode($facturaE->GetVoucherTypes()), true);
+        
+    }
+    
+    public function getPersonaWSA7($data)
+    {
+        $wsa7 = $this->afip->__get('RegisterScopeSeven');
+        $res = $wsa7->getPersona($data);
+        return $res;
+        //$facturaE = json_decode(json_encode($facturaE->GetVoucherTypes()), true);
+        
+    }
+    
+    public function getWSA7Status()
+    {
+        $facturaE = $this->afip->__get('RegisterScopeSeven');
+        $res = $facturaE->GetServerStatus();
+        
+        
+        return $res;
+        //$facturaE = json_decode(json_encode($facturaE->GetVoucherTypes()), true);
+        
+    }
+    
+    
+    public function getPersonaWSA9($data)
+    {
+        $wsa9 = $this->afip->__get('RegisterScopeNine');
+        $res = $wsa9->getPersona($data);
+        return $res;
+        //$facturaE = json_decode(json_encode($facturaE->GetVoucherTypes()), true);
+        
+    }
+    
+    public function getWSA100($data)
+    {
+        $wsa100 = $this->afip->__get('RegisterScopeOneHundred');
+        
+        
+        $res = $wsa100->getParameterCollectionByName($data);
+        return $res;
+        
+    }
+   
+    
      public function getDatos($data)
     {
         $datos = $this->afip->__get($data);
